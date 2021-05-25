1f6d9e811d663ffaddf08e72e1cf9bf67158f9d0
Dhruv Goyal <84144409+goyaldhruv63@users.noreply.github.com>
2021-05-25 18:58:49 +0530

    to_find_quotient_and_remainder.C

---
+++ to_find_quotient_and_remainder.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,quotient=0,remainder=0;
+	printf("enter the value of a and b");
+	scanf("%d%d",&a,&b);
+	printf("quotient of a and b=%d",a/b);
+	printf("\nremainder of a and b=%d",a%b);
+	return 0;
+	
+}
