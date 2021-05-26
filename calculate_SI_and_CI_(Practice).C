403e55d6a220ff93d0624a36b2fa726c7e9266a3
Dhruv Goyal <84144409+goyaldhruv63@users.noreply.github.com>
2021-05-26 18:05:22 +0530

    calculate_SI_and_CI_(Practice).C

---
+++ calculate_SI_and_CI_(Practice).C
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+
+int main()
+{
+	float p,t,r,si,ci;
+	printf("enter  principal amount (p): ");
+	scanf("%f",&p);
+	printf("enter time in year(t): ");
+	scanf("%f",&t);
+	printf("enter rate in percent(r): ");
+	scanf("%f",&r);
+	
+	si=(p*t*r)/100.0;
+	ci=p * (pow(1+r/100,t ));
+	
+	printf("simple interest=%0.3f\n",si);
+	printf("compound interest=%0.3f",ci);
+	
+	return (0);
+}
